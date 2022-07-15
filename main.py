730c78c40cc29fa4107e1290362394ced10c4235
Silentninja23 <alibugrahim06@gmail.com>
2022-07-14 20:48:13 -0400

    just a test

--- main.py
+++ main.py
@@ -7,4 +7,26 @@ import finviz
 #Variables
 headers = {}
 
-import this
+#import this
+
+
+while True:
+    city = input('What City?\n>>> ')
+    if city.isalpha():
+        break
+    else:
+        print('That is not a vailid input!')
+
+while True:
+    city = input('What Month?\n>>> ')
+    if city.isalpha():
+        break
+    else:
+        print('That is not a vailid input!')
+
+while True:
+    city = input('What day of the week?\n>>> ')
+    if city.isalpha():
+        break
+    else:
+        print('That is not a vailid input!')
